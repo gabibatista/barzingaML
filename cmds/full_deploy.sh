80bd8dc6d8d5d00c61d4221245aec52404a864a3
Luan Nico <luanpotter27@gmail.com>
2019-04-10 21:35:07 -0300

    .

--- cmds/full_deploy.sh
+++ cmds/full_deploy.sh
@@ -30,6 +30,5 @@ gcloud compute instances create barzinga-ml \
     --machine-type=g1-small \
     --scopes userinfo-email,cloud-platform \
     --metadata-from-file startup-script=cmds/gce_startup_script.sh \
-    --tags http-server \
-    --tags https-server \
+    --tags http-server,https-server \
     --network-interface address=$ip
